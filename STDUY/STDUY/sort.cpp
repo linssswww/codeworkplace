d6d59fc1307445d9a49d2c015abf365724837816
theliang <497598399@qq.com>
2018-07-21 13:56:20 +0800

    1、增加sort顺序插入排序， 利用该insertSort遍历排序。

--- STDUY/STDUY/sort.cpp
+++ STDUY/STDUY/sort.cpp
@@ -1,46 +1,50 @@
 #include<vector>
 using namespace std;
+void sort(int val[], int key) {
+	int value = val[key];
+	//发现标志位
+	int sign = 0;
+	bool isfind = false;
+	for (int i = 0;i < key;i++) {
+		if (value < val[i]) {
+
+			sign = i;
+			isfind = true;
+			break;
+		}
+	}
+
+	if (isfind) {
+		int tmp = 0;
+		for (int i = key; i > sign;i--) {
+			val[i] = val[i - 1];
+		}
+		val[sign] = value;
+		//往后退一格
+	}
+}
 void InsertSort(int val[], int size) 
 {
 	int key = 0;
 	for (int i = 1; i < size ; i++) 
 	{
-		key = i;
 		//对key的前一个数组串进行排序
-		//sort()
-		
-		
+		sort(val, i);
 	}
 }
 //顺序排序
-void sort(int val[], int key) {
-	int value = val[key];
-	//发现标志位
-	int sign = 0;
-	for (int i = 0;i < key;i++) {
-		if (value < val[i]) {
-			
-			sign = i;
-			break;
-		}
-	}
 
-	
-	int tmp = 0;
-	for (int i = key; i > sign;i--) {
-		val[i] = val[i-1];
-	}
-	val[sign] = value;
-	//往后退一格
-
-
-}
-
+void sort(int val[], int key);
+void InsertSort(int val[], int size);
 void main() 
 {
-	int val[] = {1,3,6,10,5};
-	sort(val, 4);
-	for (int i = 0;i < 5;i++) {
+	int val[] = {1,5,6,10,2,100,9,20,21,15};
+
+	int size = 10;
+	InsertSort(val, size);
+
+	for (int i = 0; i < size; i++)
+	{
 		printf(" %d", val[i]);
 	}
 	getchar();
